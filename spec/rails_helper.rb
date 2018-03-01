c61df29ad3bba8747b26b3f944d2e0e00a740939
leodotng <josh@leo.ng>
2018-02-28 21:54:38 -0700

    config rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,3 +55,27 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+require 'database_cleaner'
+
+Shouilda:Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+RSpec.configuration do |config|
+  config.include FactoryBot::Syntax::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+end
